5b9548b1a020e5e3eb38b9b2b50093b5d31dd41d
Shahan Neda <shahan.neda@gmail.com>
2020-04-29 12:18:03 -0700

    added linear intropolationkKJ

--- utility.py
+++ utility.py
@@ -9,16 +9,36 @@ class BasicObject:
             self.scene.frames.append([])
         self.scene.frames[i].append([function, parameters]); 
 
-    
-class Shape(BasicObject):
-    def move_to(self, point, time, starting_time=0):
+    def get_anim_frames(self, time, starting_time):
         startingFrame = starting_time * self.scene.FRAME_RATE
         totalFrames = startingFrame + time * self.scene.FRAME_RATE;
+        return (startingFrame, totalFrames)
     
-        for i in range(int(startingFrame), int(totalFrames)):
-            prog = (i-startingFrame)/(int(totalFrames)-int(startingFrame));
-            self.insert_animation_into_frame(self.change_coord, [Point(point.x * prog, point.y * prog)], i ); 
+    def get_prog(self,index, startingFrame, lastFrame):
+        if(index >= startingFrame):
+            pass
+            #return 1
+        return float((index-int(startingFrame))/(int(lastFrame)-int(startingFrame)))
+
+def lin_interpolate(x1, y1, x2, y2, x3):
+    return y1 + (x3 - x1) * ((y2-y1)/(x2-x1))
+
+class Shape(BasicObject):
+    def move_to(self, point, duration, starting_time=0):
+        startingFrame , lastFrame = self.get_anim_frames(duration, starting_time); 
+        for i in range(int(startingFrame), int(lastFrame)):
+            prog = self.get_prog(i, startingFrame, lastFrame)
+
+            self.insert_animation_into_frame(self.lerp_to, [point, startingFrame, lastFrame,i], i ); 
     
+    def lerp_to(self, final_point, first_frame, last_frame, current_frame_index):
+            lerped_x = lin_interpolate(first_frame, self.x, last_frame, final_point.x, current_frame_index) 
+            lerped_y = lin_interpolate(first_frame, self.y, last_frame, final_point.y, current_frame_index)
+            self.change_coord(Point(lerped_x, lerped_y));
+
+            print(f"final point = {final_point.x} {final_point.y} current pos = {self.x} {self.y}" )
+            print(f"moving to {lerped_x} y {lerped_y}");
+
     def change_coord(self, point):
         self.setX(point.x);
         self.setY(point.y);
