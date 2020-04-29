cc7b27d9e5cb0500ed115f9cff66ce1bd5fa68a1
Shahan Neda <shahan.neda@gmail.com>
2020-04-28 22:30:57 -0700

    added other util

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import math
 import cairo
 import utility
 from utility import *
+import os
 
-WIDTH, HEIGHT = 256, 256
+WIDTH, HEIGHT = 1000, 1000
 
 objectsToDraw = []
-objectsToDraw.append(BasicObject([Point(0,0), Point(100,0), Point(100,100), Point(0,100) ]));
+#objectsToDraw.append(BasicObject([Point(0,0), Point(100,0), Point(100,100), Point(0,100)], Color(0,1,0)))
 
-objectsToDraw.append(Rectangle(200,200,20,20, color=Color.))
+objectsToDraw.append(Rectangle(200,200,20,20,Color(1,0,0) ))
+objectsToDraw.append(Rectangle(200,100,20,20,Color(1,1,0) ))
+objectsToDraw.append(Rectangle(100,100,20,20,Color(0.2,1,0) ))
 
 surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
 ctx = cairo.Context(surface)
@@ -16,26 +19,28 @@ ctx = cairo.Context(surface)
 ctx.rectangle(0, 0, WIDTH, HEIGHT)
 ctx.set_source_rgb(0.8, 0.8, 1)
 ctx.fill()
+def drawFrame():
+    for object in objectsToDraw:
+        ctx.move_to(object.points[0].x, object.points[0].y)
 
-for object in objectsToDraw:
-    ctx.move_to(object.points[0].x, object.points[0].y)
+        for point in object.points[1:]: # skip first point since we already moved there
+            ctx.line_to(point.x, point.y)
+        ctx.close_path()
+        ctx.set_source_rgb(object.color.r, object.color.g, object.color.b)
+        ctx.fill()
 
-    for point in object.points[1:]: # skip first point since we already moved there
-        ctx.line_to(point.x, point.y)
-    ctx.close_path()
+def writeFrame(index):
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'animFrames/frame' + str(index) + ".png")
+    surface.write_to_png(filename)
 
 
-ctx.set_source_rgb(1, 0, 0)
-ctx.fill()
-
-
-
+drawFrame();
+writeFrame(1);
 
 #ctx.rectangle(0, 0, 50, 120)
 #ctx.set_source_rgb(1, 0, 0)
 #ctx.fill()
-
-surface.write_to_png("example.png")
 
 
 
--- utility.py
+++ utility.py
@@ -1,18 +1,21 @@
 class BasicObject:
-    def __init__(self, points):
+    def __init__(self, points, color):
         self.points = points
+        self.color = color
 
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-class Rectangle(BasicObject):
-    def __init__(self, x, y, width, height):
-        super().__init__(points=[Point(x,y), Point(x+width, y), Point(x+width, y+height), Point(x,y+height)]);
-        
 class Color:
-    def __init__(r, g, b):
+    def __init__(self, r, g, b):
         self.r = r
         self.g = g
         self.b = b
+
+class Rectangle(BasicObject):
+
+    def __init__(self, x, y, width, height, color=Color(0,0,0)):
+        super().__init__( [Point(x,y), Point(x+width, y), Point(x+width, y+height), Point(x,y+height)] , color);
+        
